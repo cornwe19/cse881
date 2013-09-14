731457451fa8b9b8b5fb309eede269a262f1e2e8
Dennis Cornwell <cornwe19@msu.edu>
2013-09-14 12:32:35 -0400

    Actually plot the data in question 2a

--- hw2/problem2.m
+++ hw2/problem2.m
@@ -1,11 +1,16 @@
 disp( 'Loading data into matrix A...' );
 A = load( 'data/2d_linear.txt' );
+newData = [0.0020 0.2223; 0.2642 0.3190; 0.5061 0.7418; 0.8912 0.8517];
+
+figure;
+hold on;
+scatter( A(:,1), A(:,2), 'red.');
+scatter( newData(:,1), newData(:,2), 'blue*' );
 
 [V,D] = eigs( cov( A ), 1 );
 
 projA = A * V;
 
-newData = [0.0020 0.2223; 0.2642 0.3190; 0.5061 0.7418; 0.8912 0.8517];
 
 disp( 'New data projected onto 1st principal component' );
 newData * V
