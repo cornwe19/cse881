af8014731f76c69a565758b95fb8ef53a307ce71
Cornwell, Dennis <d.cornwell@techsmith.com>
2013-10-21 22:22:38 -0400

    Datamerge works or something

--- project1/881Project/src/cse/datamining/DataMerge.java
+++ project1/881Project/src/cse/datamining/DataMerge.java
@@ -4,11 +4,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class DataMerge {
-   private static HashMap<Integer, String> sTrainingData = new HashMap<Integer, String>(); 
+   private static HashMap<Integer, ArrayList<String>> sTrainingData = new HashMap<Integer, ArrayList<String>>(); 
    
    public static void main( String[] argv ) throws IOException {
       if ( argv.length < 3 ) {
@@ -17,54 +18,73 @@ public class DataMerge {
       }
       
       BufferedReader trainReader = new BufferedReader( new FileReader( argv[0] ) );
+      FileWriter       writer = new FileWriter( argv[2] );
+      String inline = trainReader.readLine();
+      int lines = 0; 
       
       System.out.printf( "Scanning '%s'\n", argv[0] );
-      
-      int lines = 0; 
-      String line = trainReader.readLine();
-      while ( line != null ) {
-         Scanner scanner = new Scanner( line );
-         
-         sTrainingData.put( scanner.nextInt(), scanner.nextLine() );
-         
-         line = trainReader.readLine();
-         
-         lines++;
-         System.out.printf( "\r%d lines read", lines );
-      }
-      
-      trainReader.close();
-      
-      System.out.printf( "\nMerging with '%s' and saving to '%s'\n", argv[1], argv[2] );
-      
-      BufferedReader   profileReader = new BufferedReader( new FileReader( argv[1] ) );
-      FileWriter       writer = new FileWriter( argv[2] );
-      
-      if ( argv.length > 3 ) {
-         writer.write( argv[3] + "\n" );
-      }
-      
-      lines = 0;
-      int saved = 0;
-      line = profileReader.readLine();
-      while( line != null ) {
-         Scanner scanner = new Scanner( line );
-         
-         String rec = sTrainingData.get( scanner.nextInt() );
-         if ( rec != null ) {
-            writer.write( String.format( "%s%s\n", line, rec ) );
-            saved++;
+      while ( inline != null ) {         
+         while ( inline != null ) {
+            Scanner scanner = new Scanner( inline );
+            
+            int uid = scanner.nextInt();
+            if ( sTrainingData.containsKey( uid ) ) {
+               sTrainingData.get( uid ).add( scanner.nextLine() );
+            } else {
+               ArrayList<String> userRecords = new ArrayList<String>();
+               userRecords.add( scanner.nextLine() );
+               sTrainingData.put( uid, userRecords );
+            }
+            
+            inline = trainReader.readLine();
+            
+            // Process it all, clear it and try again
+            if ( sTrainingData.size() > 100000 ) {
+               System.out.print( "\nChunk read, processing" );
+               break;
+            }
+                       
+            lines++;
+            System.out.printf( "\r%d lines read", lines );
          }
          
-         line = profileReader.readLine();
-         lines++;
+         System.out.printf( "\nMerging with '%s' and saving to '%s'\n", argv[1], argv[2] );
          
-         System.out.printf( "\r%d lines read %d lines saved", lines, saved );
+         BufferedReader   mergeReader = new BufferedReader( new FileReader( argv[1] ) );
+         
+         if ( argv.length > 3 ) {
+            writer.write( argv[3] + "\n" );
+         }
+         
+         lines = 0;
+         int saved = 0;
+         String mline = mergeReader.readLine();
+         while( mline != null ) {
+            Scanner scanner = new Scanner( mline );
+            
+            ArrayList<String> recs = sTrainingData.get( scanner.nextInt() );
+            if ( recs != null ) {
+               for ( String rec : recs ) {
+                  writer.write( String.format( "%s%s\n", mline, rec ) );
+                  saved++;
+               }
+            }
+            
+            mline = mergeReader.readLine();
+            lines++;
+            
+            System.out.printf( "\r%d lines read %d lines saved", lines, saved );
+         }
+         mergeReader.close();
+      
+         System.out.println( "\nChunk processed" );
+         
+         sTrainingData.clear();
       }
       
       System.out.println( "\nDone" );
       
-      profileReader.close();
+      trainReader.close();
       writer.close();
    }
 }
